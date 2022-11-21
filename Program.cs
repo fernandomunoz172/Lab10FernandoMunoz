db38e8df313e768ad6024ad15562f45245380a2f
fernandomunoz172 <munozf312@gmail.com>
2022-11-20 21:57:11 -0700

    Lab10

--- Program.cs
+++ Program.cs
@@ -10,9 +10,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
             List<string> activities = new List<string>();
             List<DateTime> initialtime = new List<DateTime>();
             List<string> StartedActivities = new List<string>();
-            
-            
-                    
+            List<DateTime> completedtime = new List<DateTime>();
+            List<string> CompletedActivities = new List<string>();
+
+
             while (true)
             {
                 int menu = Menu();
@@ -24,9 +25,21 @@ namespace MyApp // Note: actual namespace depends on the project name.
                     case 2:
                         StartActivity(ref activities, ref StartedActivities, ref initialtime);
                         break;
+                    case 3:
+                        CompleteActivity(ref activities,ref StartedActivities, ref CompletedActivities, ref completedtime);
+                        break;
                     case 4:
+
+                        RemoveActivity(ref activities, ref StartedActivities, ref CompletedActivities);
+                        break;
+                    case 6:
                         Stats(ref activities);
                         break;
+   
+                }
+                if (menu == 7)
+                {
+                    break;
                 }
             }
         }
@@ -50,15 +63,19 @@ namespace MyApp // Note: actual namespace depends on the project name.
                     WriteLine("2. Start an activity.");
                     ForegroundColor = ConsoleColor.DarkMagenta;
                     WriteLine("3. Complete an activity");
-                    ForegroundColor = ConsoleColor.DarkBlue;
-                    WriteLine("4. See Statistics");
+                    ForegroundColor = ConsoleColor.Cyan;
+                    WriteLine("4. Remove an activity");
+                    ForegroundColor = ConsoleColor.DarkYellow;
+                    WriteLine("5. See current List");
+                    ForegroundColor = ConsoleColor.Magenta;
+                    WriteLine("6. See statitics");
                     ForegroundColor = ConsoleColor.Red;
-                    WriteLine("4. Exit");
+                    WriteLine("7. Exit");
                     ForegroundColor = ConsoleColor.DarkGray;
                     Write("Type the number of the action you wamt to do: ");
                     ForegroundColor = ConsoleColor.White;
                     int choice = Convert.ToInt32(ReadLine());
-                    if (choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5)
+                    if (choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5 || choice == 6)
                     {
                         Clear();
                         return choice;
@@ -176,7 +193,6 @@ namespace MyApp // Note: actual namespace depends on the project name.
                     while (true)
                     {
                         AskforActivityStarting(ref activities, ref StartedActivities, ref initialtime);
-                        ReadLine();
                         break;
                     }
                     break;
@@ -186,7 +202,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             }
         }
-        public static void AskforActivityStarting(ref List<string> activities, ref List<string>   StartedActivities, ref List<DateTime> initialtime)
+        public static void AskforActivityStarting(ref List<string> activities, ref List<string> StartedActivities, ref List<DateTime> initialtime)
         {
 
             while (true)
@@ -238,5 +254,153 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 }
             }
         }
+        static void CompleteActivity(ref List<string> activities, ref List<string> StartedActivities, ref List <string>CompletedActivities, ref List<DateTime> completedTime)
+        {
+            while (true)
+            {
+                if (activities.Count==0)
+                {
+                     ForegroundColor = ConsoleColor.White;
+            string title = "STARTED ACTIVITIES";
+            ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(@$"{title,50}");
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("No activities yet");
+                    ForegroundColor = ConsoleColor.DarkGray;
+                    Write("Press any key to go back: ");
+                    ReadKey();
+                    Clear();
+                    break;
+                }
+                if (StartedActivities.Count==0&&activities.Count>0)
+                {
+                     ForegroundColor = ConsoleColor.White;
+            string title = "STARTED ACTIVITIES";
+            ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(@$"{title,50}");
+                     ForegroundColor = ConsoleColor.Red;
+                    WriteLine("You have not started an activity yet. Go back to the menu to start an activity.");
+                    ForegroundColor = ConsoleColor.DarkGray;
+                    Write("Press any key to go back: ");
+                    ReadKey();
+                    Clear();
+                    break;
+                }
+                else 
+                {
+                while (true)
+                {
+AskForActivityCompleting(ref StartedActivities, ref CompletedActivities, ref completedTime);
+break;
+                }
+                break;
+            
+        }}
+        }
+        static void AskForActivityCompleting(ref List<string> StartedActivities, ref List<string> CompletedActivities, ref List<DateTime> completedTime)
+        {
+            while(true)
+            {
+                 string title = "STARTED ACTIVITIES";
+            ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(@$"{title,50}");
+                ForegroundColor = ConsoleColor.DarkGray;
+                WriteLine("0.- GO BACK");
+                try{
+            for (int x = 0; x < StartedActivities.Count; x++)
+            {
+                ForegroundColor = ConsoleColor.White;
+                WriteLine($"{x + 1}.- {StartedActivities[x]}");
+            }
+             ForegroundColor = ConsoleColor.White;
+                    Write("Put the number of the activity you would like to complete: ");
+                    int i = Convert.ToInt32(ReadLine());
+                    if (i == 0)
+                    {
+                        Clear();
+                        break;
+                    }
+                    if (i > StartedActivities.Count)
+                    {
+                        Clear();
+                        ForegroundColor = ConsoleColor.Red;
+                        WriteLine("That is not within the range. Try again");
+                        continue;
+                    }
+                     else
+                    {
+
+                        Clear();
+                        for (int x = i - 1; x < StartedActivities.Count;)
+                        {
+                            CompletedActivities.Add(StartedActivities[x]);
+                            completedTime.Add(DateTime.Now);
+                            break;
+                        }
+                    }
+        }
+        catch{
+             Clear();
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("That is not valid. Try again");
+                    continue;
+        }}}
+        public static void RemoveActivity(ref List<string> activities, ref List<string> StartedActivities, ref List<string> CompletedActivities)
+        {
+            while (true)
+            {
+                try
+                {
+                    
+                    ForegroundColor = ConsoleColor.DarkYellow;
+                    WriteLine("REMOVE ACTIVITIES");
+                    ForegroundColor = ConsoleColor.White;
+                    WriteLine("1.-Remove ALL the activities");
+                    WriteLine("2.-Remove a specific activity ");
+                    WriteLine("3.-Back");
+                    ForegroundColor = ConsoleColor.DarkGray;
+                    Write("Type the number of the action you wamt to do: ");
+                    ForegroundColor = ConsoleColor.White;
+                    int choice = Convert.ToInt32(ReadLine());
+                    
+                    if (choice == 1)
+                    {
+                        Clear();
+                        activities.Clear();
+                        StartedActivities.Clear();
+                        CompletedActivities.Clear();
+                        WriteLine("Everything is clear. Press any key to continue");
+                        ReadKey();
+                        break;
+                    }
+                    if (choice == 2)
+                    {
+
+                    }
+                    if (choice == 3)
+                    {
+                        Clear();
+                        break;
+                    }
+                    else
+                    {
+                        Clear();
+                        ForegroundColor = ConsoleColor.Red;
+                        WriteLine("That is not in the options. Try again");
+                    }
+                }
+                catch
+                {
+                    Clear();
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("That is not in the options. Try again");
+                }
+            }
+        }
+        public static void SeeCurrentList()
+        {
+            
+        }
     }
 }
+
